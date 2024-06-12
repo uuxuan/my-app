aecfaba2a35bbb9ddabb04914f3dc28f885833e5
uuxuan <113320601+uuxuan@users.noreply.github.com>
2024-06-12 12:58:08 +0800

    0612

--- src/components/tic-tac-toe/TicTacToe.js
+++ src/components/tic-tac-toe/TicTacToe.js
@@ -1,28 +1,45 @@
-import React, {useState} from 'react';
-import Board from './Board';
+import React, { useState } from "react";
+import Board from "./Board";
 
-function TicTacToe(){
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
+function TicTacToe() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
-    const handlePlay = (nextSquares) =>{
-        const nextHistory = [...history.slice(0, currentMove+1), nextSquares];
-        setHistory(nextHistory);
-        setCurrentMove(nextHistory.length - 1);
-    }
+  const handlePlay = (nextSquares) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
 
-    return(
-        <div className='game'>
-            <div className='game-board'>
-                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-            </div>
-            <div className='game-info'>
-                <h4>遊戲歷程</h4>
-            </div>
-        </div>
-    );
+
+  const moves = history.map((squares, move) => {
+    const jumpTo = (nextMove) => setCurrentMove(nextMove);
+    let description = move > 0 ? `回到第 ${move} 步` : "遊戲開始";
+    // if (move > 0) {
+    //   description = "回到第 " + move + " 步";
+    // } else {
+    //   description = "遊戲開始";
+    // }
+    return <>
+      <li key = {move}>
+        <button onClick={()=> jumpTo(move)}>{description}</button>
+      </li>
+    </>;
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <h4>遊戲歷程</h4>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
 }
 
 export default TicTacToe;
